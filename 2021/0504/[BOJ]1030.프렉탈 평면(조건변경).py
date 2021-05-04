dd7eab68f58d38988d0ff3d99e2f4e38eec32f55
이동희 <ldh29768@gmail.com>
2021-05-04 21:05:30 +0900

    4월폴더/[BOJ]프렉탈평면(MO),약수

---
+++ 2021/0504/[BOJ]1030.프렉탈 평면(조건변경).py
@@ -0,0 +1,43 @@
+def dfs(matrix, k):
+  if k == s:
+    return matrix
+
+  matrix_length = len(matrix)*N
+  new_matrix = list([0]*(matrix_length) for _ in range(matrix_length))
+
+  for i in range(len(matrix)):
+    for j in range(len(matrix)):
+      convert_matrix = one_convert_to if matrix[i][j] else zero_convert_to
+      start_i = N*i
+      start_j = N*j
+      for m in range(N):
+        for n in range(N):
+          if start_j+n > C2 or start_i+m > R2:
+            break
+          new_matrix[start_i+m][start_j+n] = convert_matrix[m][n]
+  return dfs(new_matrix, k+1)
+
+
+
+def main():
+  first_matrix = [[0]]
+  return dfs(first_matrix,0)
+
+
+if __name__ == "__main__":
+  s,N,K,R1,R2,C1,C2 = map(int,input().split())
+
+  one_convert_to = list([1]*N for _ in range(N))
+  zero_convert_to = list([0]*N for _ in range(N))
+  start_point = (N-K)//2
+
+  for i in range(start_point,start_point+K):
+    for j in range(start_point,start_point+K):
+      zero_convert_to[i][j] = 1
+
+  result_matrix = main()
+
+  for i in range(R1,R2+1):
+    for j in range(C1,C2+1):
+      print(result_matrix[i][j],end='')
+    print()
---
+++ 2021/0504/[BOJ]1037.약수.py
@@ -0,0 +1,7 @@
+def main():
+  nums = list(map(int,input().split()))
+  print(max(nums)*min(nums))
+
+if __name__ == "__main__":
+  N = int(input())
+  main()
