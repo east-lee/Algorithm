516fcd7b4d2f463e260e73171ebd4b7623b56510
Donghee-L <68374003+Donghee-L@users.noreply.github.com>
2021-09-30 08:58:57 +0900

    [BOJ] 작업 (G4/DP)

---
+++ BOJ/[BOJ]2056.작업.py
@@ -0,0 +1,30 @@
+def get_data():
+  N = int(input())
+  working_time = [0] * (N+1)
+  link = {}
+
+  for i in range(1, N+1):
+    working_info = list(map(int, input().split()))
+    working_time[i] = working_info[0]
+    if working_info[1] == 0:
+      continue
+    for j in working_info[2:]:
+      if i not in link:
+        link[i] = [j]
+      else:
+        link[i].append(j)
+        
+  return [N, working_time, link]
+
+
+if __name__ == "__main__":
+  N, working_time, link = get_data()
+
+  for i in range(1, N+1):
+    if i in link:
+      t = 0
+      for j in link[i]:
+        t = max(t, working_time[j])
+      working_time[i] += t
+  # print(working_time)
+  print(max(working_time))
